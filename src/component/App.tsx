55fbd84f2f03c240fdf33b45e33607ca80c48df5
IgorKramskoy <ig.kramskoy@it-justice.com>
2023-04-12 21:33:55 +0300

    test webpack config

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './style.scss'
 const App = () => {
+    const [text, setText] = useState('');
+    const [isShow, setShow] = useState(false);
+    console.log(isShow)
+
     return (
         <div className="header">
             React
+            <input
+                onChange={(e) => {
+             setText(e.target.value)
+            }}
+                value={text}
+            />
+            <button onClick={() => setShow(true) }>Show Text</button>
+            <button onClick={() => setText('') }>Delete Text</button>
+            {
+                isShow &&
+                    <div>
+                        {text}
+                    </div>
+            }
         </div>
     );
 };
